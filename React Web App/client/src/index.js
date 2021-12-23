a8fbfde27c909d3829df7a5599f6c00a323f43fd
Timothy Kemmis <kemmojr@gmail.com>
2021-12-23 15:23:26 +1100

    Added client directory and contents

---
+++ React Web App/client/src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Button, Container, Card, Row } from 'react-bootstrap'
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
